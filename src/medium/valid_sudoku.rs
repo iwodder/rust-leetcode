2f3ad881fcb63b4f10f031b9de9e8165d8454eb7
iwodder <ian.wodder@gmail.com>
2020-01-26 21:16:41 -0600

    ..

--- src/medium/valid_sudoku.rs
+++ src/medium/valid_sudoku.rs
@@ -13,7 +13,8 @@ impl Point {
 
 pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
   let mut points: HashMap<char, Vec<Point>> = HashMap::new();
-  for r in 0..board.len() {
+  let mut valid = true;
+  'outer: for r in 0..board.len() {
     for c in 0..board[r].len() {
       if board[r][c] != '.' {
         match points.get_mut(&board[r][c]) {
@@ -25,9 +26,38 @@ pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
           }
         }
       }
+      if (r % 3) == 0 && (c % 3) == 0 {
+        if !check_squares(&board, r, c) {
+          valid = false;
+          break 'outer;
+        }
+      }
     }
   }
-  check_points(points)
+  if valid {
+    check_points(points)
+  } else {
+    valid
+  }
+}
+
+fn check_squares(sub_board: &Vec<Vec<char>>, start_row:usize, start_col:usize) -> bool {
+  let mut nums = [false; 10];
+  let mut valid = true;
+  'outer: for r in 0..3 as usize {
+    for c in 0..3 as usize {
+      if sub_board[start_row + r][start_col + c] != '.' {
+        let x = sub_board[start_row + r][start_col + c].to_digit(10).unwrap() as usize;
+        if nums[x] != false {
+          valid = false;
+          break 'outer;
+        } else {
+          nums[x] = true;
+        }
+      }
+    }
+  }
+  valid
 }
 
 fn check_points(mut p: HashMap<char, Vec<Point>>) -> bool {
@@ -86,15 +116,38 @@ mod tests {
   }
   #[test]
   fn test_3() {
-    let v1 = vec!['8','3','.','.','7','.','.','.','.'];
+    let v1 = vec!['9','3','.','.','7','.','.','.','.'];
     let v2 = vec!['6','.','.','1','9','5','.','.','.'];
-    let v3 = vec!['.','9','8','.','.','.','.','6','.'];
+    let v3 = vec!['.','9','8','.','.','1','.','6','.'];
     let v4 = vec!['1','.','2','.','6','.','.','.','3'];
     let v5 = vec!['4','.','.','8','.','3','.','.','1'];
     let v6 = vec!['7','.','.','.','2','.','.','.','6'];
     let v7 = vec!['.','6','.','.','.','.','2','8','.'];
     let v8 = vec!['.','.','.','4','1','9','.','.','5'];
     let v9 = vec!['.','.','.','.','8','.','.','7','9'];
+    let board = vec![v1, v2, v3, v4, v5, v6, v7, v8, v9];
+    assert!(!is_valid_sudoku(board));
+  }
+  #[test]
+  fn test_4() {
+    let v1 = vec!['8','3','.'];
+    let v2 = vec!['6','.','.'];
+    let v3 = vec!['.','9','8'];
+    let board = vec![v1, v2, v3];
+    assert!(!check_squares(&board, 0, 0))
+  }
+
+  #[test]
+  fn test_5() {
+    let v1 = vec!['.','.','.','.','5','.','.','1','.'];
+    let v2 = vec!['.','4','.','3','.','.','.','.','.'];
+    let v3 = vec!['.','.','.','.','.','3','.','.','1'];
+    let v4 = vec!['8','.','.','.','.','.','.','2','.'];
+    let v5 = vec!['.','.','2','.','7','.','.','.','.'];
+    let v6 = vec!['.','1','5','.','.','.','.','.','.'];
+    let v7 = vec!['.','.','.','.','.','2','.','.','.'];
+    let v8 = vec!['.','2','.','9','.','.','.','.','.'];
+    let v9 = vec!['.','.','4','.','.','.','.','.','.'];
     let board = vec![v1, v2, v3, v4, v5, v6, v7, v8, v9];
     assert!(!is_valid_sudoku(board));
   }
