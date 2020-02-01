0788d0d113693d9eb299c240a45d000b411f305f
iwodder <ian.wodder@gmail.com>
2020-02-01 07:57:57 -0600

    ..

--- src/easy/remove_duplicates.rs
+++ src/easy/remove_duplicates.rs
@@ -1,14 +1,22 @@
 pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-  let mut insert_pos = 1;
-  for current_num in 0..(nums.len() - 1) {
-    insert_pos = current_num + 1;
-    let mut next_num = current_num + 1;
-    while nums[current_num] == nums[next_num] {
-      next_num += 1;
+  if nums.len() > 0 {
+    let mut length = 1;
+    let mut next_num = 0;
+    for current_num in 0..(nums.len() - 1) {
+      while next_num < nums.len() && nums[current_num] == nums[next_num] {
+        next_num += 1;
+      }
+      if next_num < nums.len() {
+        nums[current_num + 1] = nums[next_num];
+        length += 1;
+      } else {
+        break;
+      }
     }
-    nums[insert_pos] = nums[next_num];
+    length
+  } else {
+    0
   }
-  2
 }
 
 #[cfg(test)]
@@ -25,5 +33,11 @@ mod test{
   fn test_2() {
     let mut v = vec![0,0,1,1,1,2,2,3,3,4];
     assert_eq!(5, remove_duplicates(&mut v));
+  }
+
+  #[test]
+  fn test_3() {
+    let mut v = vec![];
+    assert_eq!(0, remove_duplicates(&mut v));
   }
 }
